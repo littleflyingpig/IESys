021d62e19cf72fa2023bef966db720a7367c3770
littleflyingpig <littleflyingpig@zyq.localdomain>
2026-05-31 18:16:51 +0800

    添加统计功能

--- IESys_/urls.py
+++ IESys_/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('expenditure', views.expenditure, name='expenditure'),
     path('income_detail/', views.income_detail, name='income_detail'),
     path('expenditure_detail/', views.expenditure_detail, name='expenditure_detail'),
+    path('week_expenditure/', views.week_expenditure, name='week_expenditure'),
+    path('month_ie/', views.month_ie, name='month_ie'),
 ]
--- IESys_/views.py
+++ IESys_/views.py
@@ -4,6 +4,9 @@ from django.contrib import messages
 from django.utils import timezone
 from .form import IeSysForm
 from .models import IeSys
+
+from datetime import date, timedelta
+import calendar
 import pandas as pd
 import plotly.express as px
 # Create your views here.
@@ -83,3 +86,65 @@ def expenditure_detail(request):
     else:
         context = {'flag': 0}
         return render(request, 'IESys_/expenditure_detail.html', context)
+    
+def week_expenditure(request):
+    """编写统计页面中的周统计"""
+    today = date.today()
+    monday = today - timedelta(days=today.weekday())
+    sunday = monday + timedelta(days=6)
+    week = ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+    amount = []
+    while monday <= sunday:
+        objects = IeSys.objects.filter(date=monday)
+        if not objects:
+            amount.append(0)
+        else:
+            total = 0
+            for object in objects:
+                total += object.expenditure_amount
+            amount.append(total)
+        monday += timedelta(days=1)
+    df = pd.DataFrame({
+        '本周': week,
+        '支出': amount
+    })
+    fig = px.bar(df, x='本周', y='支出', title='周支出统计')
+    fig = fig.to_html(full_html=False)
+    context = {'fig': fig}
+    return render(request, 'IESys_/week_expenditure.html', context)
+
+def month_ie(request):
+    """编写统计页面中的月收支"""
+    today = date.today()
+    total_days = calendar.monthrange(today.year, today.month)[1]
+    first_day = date(today.year, today.month, 1)
+    days = []
+    amount = []
+    for i in range(total_days):
+        cnt = first_day + timedelta(days=i)
+        
+        days.append(str(cnt.day))
+        objects = IeSys.objects.filter(date=cnt)
+        if objects:
+            total = 0
+            for object in objects:
+                total += object.income_amount - object.expenditure_amount
+            if i == 0:
+                amount.append(total)
+            else:
+                total += amount[i-1]
+                amount.append(total)
+        else:
+            if i == 0:
+                amount.append(0)
+            else:
+                amount.append(amount[i-1])
+    data = pd.DataFrame({
+        '日': days,
+        '总收支': amount
+    })
+    fig = px.bar(data, x='日', y='总收支', title=str(today.month)+'月总收支统计')
+    fig.update_xaxes(title_text=str(today.month)+'月')
+    fig = fig.to_html(full_html=False)
+    context = {'fig': fig}
+    return render(request, 'IESys_/month_ie.html', context)
